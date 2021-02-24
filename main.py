d2b532d7205eae975d95d5837d745335db72ca6d
Livia Brookes <liviab1302@gmail.com>
2021-02-24 18:02:02 +0000

    added manager scores

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for i in range(numDevelopers):
     if line[0] not in companies:
         companies.append(line[0])
         companyDevelopers.append([])
-    developer.append(line[1])
+    developer.append(int(line[1]))
     developer.append(line[3:])
     developers.append(developer)
     index = companies.index(line[0])
@@ -37,8 +37,11 @@ numManagers = int(data.readline())
 managers = []
 for i in range(numManagers):
     line = data.readline().split()
-    line[-1] = int(line[-1])
-    managers.append(line)
+    details = []
+    details.append(i)
+    details.append(line[0])
+    details.append(int(line[1]))
+    managers.append(details)
     managersBonus.append(line)
 managersBonus.sort(key= lambda x:x[1])
 managersBonus.reverse()
@@ -59,27 +62,51 @@ def workPotential(dev1, dev2):
                 distinct+=1
     return wp*(distinct-wp)
 
-<<<<<<< Updated upstream
 def bonusPotential(dev1,dev2):
-    if dev1[1] == dev2[1]:
-        return int(dev1[2])*int(dev2[2])
+    if dev1[1] ==dev2[1]:
+        return dev1[2]*dev2[2]
+    return 0
 
 def totalPotential(dev1, dev2):
     return workPotential(dev1, dev2) + bonusPotential(dev1, dev2)
 
-print(totalPotential(developers[0],developers[2]))
-=======
-def finingAdjseats(plan):
-    seatnum=[]
-    for a in range(len(plan)):
-        for b in range(len(plan[a])):
-            if plan [a][b] == '_':
-                coordval=[]
-                ajval=0
-                coordval.append(a)
-                coordval.append(b)
-                if plan[a-1][b] == '':
+def managerScore(managers):
+    # probably needs to be redone :/
+    scores = []
+    for managerProfile in managers:
+        managerScores = []
+        developerArray = []
+        managerArray = []
+        for person in developers:
+            devScore= [person[0]]
+            devScore.append(bonusPotential(managerProfile,person))
+            developerArray.append(devScore)
+        for person in managers:
+            # includes all managers (including itself)
+            manScoreArr = [person[0]]
+            if person != managerProfile:
+                manScoreArr.append(bonusPotential(managerProfile,person))
+            else:
+                manScoreArr.append(0)
+            managerArray.append(manScoreArr)
+        managerScores.append(developerArray)
+        managerScores.append(managerArray)
+        scores.append(managerScores)
+    return scores
+    # first array = first manager - first part of this array is scores w developers, second part is scores w managers
+
+print(managerScore(managers))
+
+# def finingAdjseats(plan):
+#     seatnum=[]
+#     for a in range(len(plan)):
+#         for b in range(len(plan[a])):
+#             if plan [a][b] == '_':
+#                 coordval=[]
+#                 ajval=0
+#                 coordval.append(a)
+#                 coordval.append(b)
+                # if plan[a-1][b] == '':
 
 
 
->>>>>>> Stashed changes
